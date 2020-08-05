a5f109e15af6c286196a08cfdc59633772f8c8c4
YH-Web-Space <yh.web.space@gmail.com>
2020-08-05 23:03:40 +0300

    faq

--- assets/js/main.js
+++ assets/js/main.js
@@ -74,3 +74,55 @@ function initComparisons() {
         }
     }
 }
+
+// Плавное появление
+const animItems = document.querySelectorAll('.anim-items');
+
+if (animItems.length > 0){
+    window.addEventListener("scroll", animOnScroll);
+    function animOnScroll() {
+        for (let i = 0; i < animItems.length; i++){
+            const animItem = animItems[i];
+            const animItemHeight = animItem.offsetHeight;
+            const animItemOffset = offset(animItem).top;
+            const animStart = 4;
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+            if (animItemHeight > window.innerHeight){
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            }
+
+            if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)){
+                animItem.classList.add('_active');
+            }else {
+                if (!animItem.classList.contains("anim-no-hide")){
+                    animItem.classList.remove('_active');
+                }
+            }
+        }
+    }
+    function offset(el) {
+        const rect = el.getBoundingClientRect(),
+            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return {top: rect.top + scrollTop, left: rect.left + scrollLeft};
+    }
+    setTimeout(()=>{
+        animOnScroll()
+    },300);
+}
+
+let acc = document.querySelectorAll(".faq__question");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        let panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
